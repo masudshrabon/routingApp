328a04aba0d8975dc2bd401dff3156b0dffc909b
Masudur Rahman <masudur.rahman@kaz.com.bd>
2018-12-03 17:49:03 +0600

    added Relative Paths in Programmatic Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes, Router, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';

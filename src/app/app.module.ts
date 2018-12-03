96841af47ca391582b9571d411e4c190ed0e62a4
Masudur Rahman <masudur.rahman@kaz.com.bd>
2018-12-03 15:10:16 +0600

    added routes to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, Router, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,6 +13,11 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServersService } from './servers/servers.service';
 import { UserComponent } from './users/user/user.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +31,8 @@ import { UserComponent } from './users/user/user.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]

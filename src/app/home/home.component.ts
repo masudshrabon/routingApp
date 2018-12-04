78ff7bdc10448fb8a65e8bf46abe4eef25dde3a2
Masudur Rahman <masudur.rahman@kaz.com.bd>
2018-12-04 19:07:38 +0600

    Passing Query Parameters and Fragments using routerLink and router navigate()

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,8 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServer(id: number) {
     // Here could be some Complex Calculations before routing
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 }

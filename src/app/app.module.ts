7b4fa74c6c4d7a7988e233e460681b959fbca37d
Masudur Rahman <masudur.rahman@kaz.com.bd>
2018-12-04 13:46:50 +0600

    Fetching Route Parameters Reactively using route params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { UserComponent } from './users/user/user.component';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent }
 ];
 @NgModule({
